ecdd265f60803c9935f98207f9aed0dc127644d5
Fernando Laube <fernando.laube@gmail.com>
2020-01-09 12:40:06 -0300

    Update files

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from drive_handler import DriveHandler
+from spreadsheet_handler import SpreadsheetHandler
+from widget_gallery import WidgetGallery
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive.metadata.readonly',
+          'https://www.googleapis.com/auth/drive.file']
+
+if __name__ == '__main__':
+
+    CREDS = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            CREDS = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not CREDS or not CREDS.valid:
+        if CREDS and CREDS.expired and CREDS.refresh_token:
+            CREDS.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            CREDS = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(CREDS, token)
+
+    spreadsheet_hdl = SpreadsheetHandler(CREDS, file_name="Expenses Tracker")
+    spreadsheet_hdl.create_spreadsheet()
+
+    appctxt = ApplicationContext()
+    gallery = WidgetGallery()
+    gallery.show()
+    sys.exit(appctxt.app.exec_())
---
+++ widget_gallery.py
@@ -0,0 +1,81 @@
+import sys
+from spreadsheet_handler import SpreadsheetHandler
+from PyQt5.QtWidgets import QApplication, QPushButton, QMessageBox,\
+                            QDoubleSpinBox, QWidget, QVBoxLayout,\
+                            QHBoxLayout, QDateEdit, QDialog,\
+                            QStyleFactory, QComboBox, QLineEdit,\
+                            QCheckBox
+from PyQt5.QtCore import QDate
+
+
+class WidgetGallery(QDialog):
+
+    def __init__(self, parent=None):
+        super(WidgetGallery, self).__init__(parent)
+
+        mainLayout = QVBoxLayout()
+
+        self.createHorizontalLayout()
+
+        submitbutton = QPushButton("Submit Expense")
+        submitbutton.clicked.connect(self.submitButtonClicked)
+
+        self.vrCheckBox = QCheckBox("Gasto com VR")
+
+        mainLayout.addLayout(self.horizontalLayout)
+        mainLayout.addWidget(self.vrCheckBox)
+        mainLayout.addWidget(submitbutton)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Expenses Tracker")
+
+        QApplication.setStyle(QStyleFactory.create("Fusion"))
+
+    def addCategories(self, new_items):
+        self.categoriesComboBox.insertItems(0, new_items)
+
+    def submitButtonClicked(self):
+        spreadsheet_hdl = SpreadsheetHandler()
+        alert = QMessageBox()
+        data = [
+            ["=MONTH(\""+self.dateEdit.date().toString("MM/dd/yyyy")+"\")",
+             self.dateEdit.date().toString("MM/dd/yyyy"),
+             str(self.doubleSpinBox.value()),
+             self.categoriesComboBox.currentText(),
+             self.specificationLine.text(),
+             self.observationLine.text()
+             ]
+        ]
+
+        if self.vrCheckBox.checkState() == 2:
+            data[0].append("VR")
+
+        spreadsheet_hdl.append_data(data, range='B4')
+        alert.setText("The expense was submitted!")
+        alert.exec_()
+
+    def createHorizontalLayout(self):
+        self.horizontalLayout = QHBoxLayout()
+
+        self.dateEdit = QDateEdit(calendarPopup=True, displayFormat="dd/MM/yy",
+                                  date=QDate.currentDate())
+
+        self.doubleSpinBox = QDoubleSpinBox(maximum=1000, decimals=2,
+                                            minimum=0)
+
+        # TODO: Find out how to get insertPolicy working
+        self.categoriesComboBox = QComboBox(insertPolicy=QComboBox.InsertAlphabetically)
+
+        # TODO: Remove hardcoded categories. Read categories from file
+        self.addCategories(["Bandejão", "Supermercado", "Contas", "Lanche",
+                            "Almoço", "Ônibus", "Outros"])
+
+        self.specificationLine = QLineEdit()
+
+        self.observationLine = QLineEdit()
+
+        self.horizontalLayout.addWidget(self.doubleSpinBox)
+        self.horizontalLayout.addWidget(self.dateEdit)
+        self.horizontalLayout.addWidget(self.categoriesComboBox)
+        self.horizontalLayout.addWidget(self.specificationLine)
+        self.horizontalLayout.addWidget(self.observationLine)
